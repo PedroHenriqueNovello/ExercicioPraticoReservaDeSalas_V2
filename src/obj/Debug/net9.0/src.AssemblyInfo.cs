a78850ef7ce371855ec82ecfbece858efa4ed549
PedroHenriqueNovello <phnporto@gmail.com>
2026-05-14 21:52:04 -0300

    docs: substitui diagrama antigo pela nova versao com padrao Builder

--- src/obj/Debug/net9.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net9.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be2fa4f922cd087cbc934831126e46cde7b0a07d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12632dfaeb53b358f555c3b6c3aabc72c8bea88d")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
